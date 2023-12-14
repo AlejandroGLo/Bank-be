de24b7d79ee5aff0804a4602cc18956355cb0ca8
Alejandro <alegalvezlo2@gmail.com>
2023-12-14 16:52:53 -0600

    Update test_routes.py

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -26,7 +26,20 @@ def test_create_account(testing_client):
     WHEN the '/accounts' page is posted to (POST)
     THEN check the response is valid
     """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
+    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€', 'country':'Spain'})
     assert response.status_code == 200
 
+def test_get_account(testing_client):
+    response = testing_client.get(
+        '/accounts/1')
+    assert response.status_code == 200
+    
 
+def test_update_account(testing_client):
+    response = testing_client.put('/accounts/1',json={'name': 'John Doe', 'currency': '€'})
+    assert response.status_code == 200
+
+def test_delete_account(testing_client):
+    response = testing_client.delete('/accounts/1')
+    assert response.status_code == 200
+    
